7a15638e62859a383445657da9b46103bf4991d0
rohhhit31 <rohitbeni31@gmail.com>
2022-05-12 00:05:50 +0530

    Front end done

--- app/src/main/java/com/example/dashboard/Parkassist.java
+++ app/src/main/java/com/example/dashboard/Parkassist.java
@@ -1,14 +1,34 @@
 package com.example.dashboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class Parkassist extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class Parkassist extends FragmentActivity implements OnMapReadyCallback {
+GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parkassist);
+        SupportMapFragment mapFragment =(SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this::onMapReady);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map =googleMap;
+        LatLng Delhi =new LatLng(28.644800, 77.216721);
+    map.addMarker(new MarkerOptions().position(Delhi).title("Delhi"));
+    map.moveCamera(CameraUpdateFactory.newLatLng(Delhi));
     }
 }
