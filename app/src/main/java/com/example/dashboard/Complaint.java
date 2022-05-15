7947242df68ac3c4cae0a234e708ea2c51ac8bec
rohhhit31 <rohitbeni31@gmail.com>
2022-05-15 20:01:29 +0530

    It's Done

--- app/src/main/java/com/example/dashboard/Complaint.java
+++ app/src/main/java/com/example/dashboard/Complaint.java
@@ -1,60 +1,72 @@
 package com.example.dashboard;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.ArrayList;
 
 public class Complaint extends AppCompatActivity {
+    EditText emailFT,passwordFT,nameFT,userNameFT,phoneFT;
+
+    Button registerBtn;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
 
-//    private ListView listView;
+
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
 
-//        listView=findViewById(R.id.listview);
-//        ArrayList<String> list= new ArrayList<>();
-//        ArrayAdapter adapter= new ArrayAdapter<String>(this,R.layout.activity_complaint,list);
-//        listView.setAdapter(adapter);
-//
-//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("ONWERSHIP");
-//        reference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                list.clear();
-//                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
-//                    Information info = snapshot.getValue(Information.class);
-//                    String txt =info.getColour() + ":" + info.getOwner() + ":" + info.getRegistered() + ":" + info.getResident() + ":" + info.getType() + ":" +info.getVehicle();
-//                            list.add(txt);
-//                }
-//                adapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+        emailFT = findViewById(R.id.emailFT);
+        passwordFT = findViewById(R.id.passwordFT);
+        nameFT = findViewById(R.id.nameFT);
+        userNameFT = findViewById(R.id.userNameFT);
+        phoneFT = findViewById(R.id.phoneFT);
+
+        registerBtn = findViewById(R.id.registerBtn);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailFT.getText().toString(), password = passwordFT.getText().toString();
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(nameFT.getText().toString(),userNameFT.getText().toString(),email,phoneFT.getText().toString(),password);
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Complaint").child(id).setValue(user);
+                            Intent intent = new Intent(Complaint.this,MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(Complaint.this, "Complaint Registered", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(Complaint.this, "Error while registering", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
-
-
-
-    }
+}
